083c9d7a3dc210a4388b4b6960a947cef80ae2af
SangeethaG1 <geeth.guntuku@gmail.com>
2024-02-14 12:04:35 -0600

    Adding Additional

---
+++ src/main/java/additional/RepeatChar.java
@@ -0,0 +1,18 @@
+package additional;
+
+public class RepeatChar {
+    public static void main(String[] args) {
+        String str = "beautiful day";
+        char[] carray = str.toCharArray();
+        System.out.println("The string is:" + str);
+        System.out.print("Repeated Characters in above string are: ");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (carray[i] == carray[j]) {
+                    System.out.print(carray[j] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
